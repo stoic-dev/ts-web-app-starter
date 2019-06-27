cefbcb8f4f125c501b1587fd30f25d982faea083
stoic-dev <john.harris297@gmail.com>
2019-06-27 16:16:56 -0600

    Use injected variable for config location.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import * as bootstrappers from './bootstrappers/bootstrappers.index';
 
+// This is defined at build time by the Webpack DefinePlugin
+declare const DEFAULT_CONFIG_PATH: string;
+
 for (const bootstrapper of Object.values(bootstrappers)) {
     if ('bootstrap' in bootstrapper && bootstrapper.bootstrap instanceof Function) {
         bootstrapper.bootstrap();
@@ -9,7 +12,7 @@ for (const bootstrapper of Object.values(bootstrappers)) {
 const container = document.createElement('div');
 
 // Fetch the application config
-fetch('/app.config.json').then(response => {
+fetch(DEFAULT_CONFIG_PATH).then(response => {
     response.json().then((config: { ENVIRONMENT: string }) => {
         container.innerText = `Environment: ${config.ENVIRONMENT}`;
         document.body.appendChild(container);
